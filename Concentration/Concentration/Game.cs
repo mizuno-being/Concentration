05c141b2714202d2692acf282524a0e2c317542a
mizuno-being <mizuno@beingcorp.co.jp>
2021-03-09 17:57:36 +0900

    Game, GameControllerクラス下書き

--- Concentration/Concentration/Game.cs
+++ Concentration/Concentration/Game.cs
@@ -7,30 +7,36 @@ using System.Threading.Tasks;
 namespace Concentration {
     public class Game {
 
-        //手番を次に回す
+        //カードをめくる OpenCard
+        //クリックされたカードのObverseをtrueにする
+
+
+        //一致判定 CheckMatchCards
+        //同じ数字だった場合
+        //  ターンプレイヤーのOwnCardsを＋2
+        //  ターンを続行
+        //  ゲーム終了判定を行う
+        //違う数字だった場合、
+        //  少し待ってからCardのObverseをfalseに
+        //  手番を次に回す
+
+
+        //手番を次に回す NextTurn
         //手番のプレイヤー番号 = プレイヤー数の場合
         //  1を返す
         //手番のプレイヤー番号 < プレイヤー数の場合
         //  手番のプレイヤー番号+1を返す
 
 
-        //めくったカードの一致判定
-        //同じ数字だった場合
-        //  CardのObverseをtrueに
-        //  ターンプレイヤーのOwnCardsを＋2
-        //違う数字だった場合、
-        //  少し待ってから裏側に戻る
-        //  次のプレイヤーの手番に移る
-
-
-        //ゲーム終了判定
+        //ゲーム終了判定 GameEndJudge
         //同じ数字だった場合に判定
         //  全てのCardのObverseがtrue
         //もしくは
         //  全てのプレイヤーのOwnCardsの合計が52
+        //      の場合、勝者判定を行う
 
 
-        //勝者判定
+        //勝者判定 WinnerJudge
         //ゲーム終了時に判定
         //  同率が3人いる場合
         //      同率が2人いる場合
--- Concentration/Concentration/GameController.cs
+++ Concentration/Concentration/GameController.cs
@@ -7,6 +7,35 @@ using System.Threading.Tasks;
 namespace Concentration {
     public class GameController {
 
+        //(input)プレイヤー数を受け取る
+        //(input)プレイヤー名を受け取る
+        //(model)プレイヤー数だけPlayerインスタンスを作成
+        //(model)プレイヤー名を各Player.Nameにセット
+        //(view) プレイヤー数を渡す
+        //(view) それぞれのプレイヤー名を渡す
+
+        //(model)52枚のカードの束が生成される
+        //(model)シャッフルメソッド
+        //(model)配置メソッド
+        //(view) カードが配置される
+
+        //≪LOOP≫
+        //(input)カードがクリックされる
+        //(model)カードをめくるメソッド
+        //(view) クリックしたカードが表側表示になる
+        //(input)カードがクリックされる
+        //(model)カードをめくるメソッド
+        //(view) クリックしたカードが表側表示になる
+        //(model)一致判定メソッド
+        //(view) 数字が同じ場合:ターンプレイヤーの獲得枚数+2
+        //(view) 数字が違った場合:少し待ってから裏側に戻す
+        //(model)(手番を次に回すメソッド)
+        //(view) 次の手番のプレイヤーを表示する
+        //(model)(終了判定メソッド)
+        //(model)(勝者判定メソッド)
+        //(view) 勝者をポップアップ表示する
+        //≪LOOP≫
+
 
     }
 }
