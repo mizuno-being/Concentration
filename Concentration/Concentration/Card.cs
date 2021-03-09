40a5d750410f14684049a6f4590375271acaa782
mizuno-being <mizuno@beingcorp.co.jp>
2021-03-09 10:49:03 +0900

    Cardクラス追加

--- Concentration/Concentration/Card.cs
+++ Concentration/Concentration/Card.cs
@@ -5,6 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Concentration {
-    class Card {
+    /// <summary>
+    /// トランプのカード
+    /// </summary>
+    public class Card {
+        /// <summary>
+        /// スート
+        /// </summary>
+        public int Suit { get; set; }
+        /// <summary>
+        /// 数字
+        /// </summary>
+        public int Rank { get; set; }
+        /// <summary>
+        /// 裏か表か
+        /// 表(数字とスートの面):true
+        /// 裏:false
+        /// </summary>
+        public bool Obverse { get; set; }
     }
 }
