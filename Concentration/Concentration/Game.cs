7617ea35a3a71eaca3acec0ba630c20838697d6c
mizuno-being <mizuno@beingcorp.co.jp>
2021-03-11 11:03:30 +0900

    Gameクラス内メソッド分割、仮作成

--- Concentration/Concentration/Game.cs
+++ Concentration/Concentration/Game.cs
@@ -13,39 +13,46 @@ namespace Concentration {
         }
 
         //一致判定 CheckMatchCards
+        public bool CheckMatchCards(Card vFirstCard, Card vSecondCard) {
+            //同じ数字だった場合trueを返す
+            //違う数字だった場合falseを返す
+        }
 
-        //同じ数字だった場合
-        //  ターンプレイヤーのOwnCardsを＋2
-        //  ターンを続行
-        //  ゲーム終了判定を行う
-        //違う数字だった場合、
-        //  少し待ってからCardのObverseをfalseに
-        //  手番を次に回す
+        //取得枚数加算 Plus
+        public int Plus(int vTurnPlayerCardNum) {
+            //一致判定がtrueの場合
+            //  ターンプレイヤーのOwnCardsを＋2
+        }
 
+        //カードを裏返す
+        public bool CloseCard(bool vObverse) {
+            //一致判定がfalseの場合
+            //  少し待ってからCardのObverseをfalseに
+        }
 
         //手番を次に回す NextTurn
-        public int NextTurn(int vTurn) { }
-        //手番のプレイヤー番号 = プレイヤー数の場合
-        //  1を返す　
-        //手番のプレイヤー番号 < プレイヤー数の場合
-        //  手番のプレイヤー番号+1を返す
-
+        public int NextTurn(int vTurn) {
+            //一致判定がfalseの場合
+            //  手番のプレイヤー番号 = プレイヤー数の場合
+            //      1を返す　
+            //  手番のプレイヤー番号 < プレイヤー数の場合
+            //      手番のプレイヤー番号+1を返す
+        }
 
         //ゲーム終了判定 JudgeGameEnd
-
-        //同じ数字だった場合に判定
-        //  全てのCardのObverseがtrue
-        //もしくは
-        //  全てのプレイヤーのOwnCardsの合計が52
-        //      の場合、ゲーム終了フラグをtrueにする
-
+        public bool JudgeGameEnd(List<Card> vDeck) {
+            //一致判定がtrueの場合
+            //  全てのCardのObverseがtrue
+            //      ゲーム終了フラグをtrueにする
+        }
 
         //勝者判定 JudgeWinner
-
-        //ゲーム終了フラグtrue時に判定
-        //  同率が3人いる場合
-        //      同率が2人いる場合
-        //          単独勝利
-        //の順に判定
+        public List<Player> JudgeWinner(List<Player> vPlayers) {
+            //ゲーム終了フラグtrue時に判定
+            //  同率が3人いる場合
+            //      同率が2人いる場合
+            //          単独勝利
+            //の順に判定
+        }
     }
 }
